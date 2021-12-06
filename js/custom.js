889739df2370f300e7ee0318797cd8317d40fc56
shanto869 <shantobiswas869@gmail.com>
2021-12-06 20:29:02 +0600

    banner ends

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	// banner slider
+	$('.banner-slider').slick({
+		prevArrow: '<i class="fas fa-chevron-left slick-prev left-arrow"></i>',
+		nextArrow: '<i class="fas fa-chevron-right slick-next right-arrow"></i>',
+		dots: true,
+		autoplay: true,
+	});
+});
